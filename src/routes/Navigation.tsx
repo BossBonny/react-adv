9672431fa97e6c61a768cf258692e5261123270e
John <johnari151298@gmail.com>
2023-11-08 15:33:53 +0100

    Component Compound Listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import {Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../logo.svg'
+import { ShoppingPage } from "../02-components-patterns/pages/ShoppingPage";
 
 const Navigation = () => {
     return (
@@ -12,7 +13,7 @@ const Navigation = () => {
 
                     <ul>
                         <li>
-                            <NavLink to="/" className={({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/" className={({isActive}) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -27,9 +28,9 @@ const Navigation = () => {
             <Routes>
                 <Route path="about" element={<h1>About Page</h1>}/>
                 <Route path="users" element={<h1>Users Page</h1>}/>
-                <Route path="/" element={<h1>Home Page</h1>}/>
+                <Route path="/" element={<ShoppingPage/>}/>
 
-                <Route path="/*" element={<Navigate to="/home" replace/>}/>
+                <Route path="/*" element={<ShoppingPage/>}/>
             </Routes>
             </div>
         </BrowserRouter>
