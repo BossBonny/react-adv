6bb088f3733198f361e7c9bf10f11ad70e53821d
John <johnari151298@gmail.com>
2023-11-09 19:26:28 +0100

    Control Props Implementado

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,15 +1,14 @@
 import { ProductCard, ProductImage, ProductTitle ,ProductButtons} from "../components"
+
+import { products } from "../data/products";
+import { useShoppingCart } from "../hooks/useShoppingCart";
 import '../styles/custom-styles.css'
-
-const product = {
-    id: '1',
-    title: 'Coffe Mug-Card',
-    img: './coffee-mug.png',
-}
-
 
 
 export const ShoppingPage = () => {
+
+    const {  shoppingCart, onProductCountChange} = useShoppingCart();
+
     return (
         <div>
             <h1>ShoppingPage</h1>
@@ -22,42 +21,49 @@ export const ShoppingPage = () => {
                 }}
             >
 
-                <ProductCard product={product} className='bg-dark text-white'>
-                    <ProductCard.Image className='custom-image'/>
-                    <ProductCard.Title title='EYO' className='text-bold'/>
-                    <ProductCard.Buttons className='custom-buttons'/>
-                </ProductCard>
+                {
+                    products.map( product => (
+                        <ProductCard 
+                            product={product} 
+                            className='bg-dark text-white'
+                            key={product.id}
+                            value={shoppingCart[product.id]?.count || 0}
+                            onChange={onProductCountChange}
+                        >
+                            <ProductImage className='custom-image'/>
+                            <ProductTitle className='text-bold' />
+                            <ProductButtons className='custom-buttons'/>
+                        </ProductCard>
+                    ))
+                }
 
-                <ProductCard product={product} className='bg-dark text-white'>
-                    <ProductImage className='custom-image'/>
-                    <ProductTitle className='text-bold' />
-                    <ProductButtons className='custom-buttons'/>
-                </ProductCard>
+            </div>
 
-                <ProductCard 
-                    product={product}
-                    style={{                        
-                        backgroundColor: '#70D1F8'
-                    }}
-                >
-                    <ProductImage
-                        style={{
-                            boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'
-                        }}
-                    />
-                    <ProductTitle 
-                        style={{
-                            fontWeight: 'bold'
-                        }}
-                    />
-                    <ProductButtons
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'end'
-                        }}
-                    />
-                </ProductCard>
+            <div className="shopping-cart">
 
+                {
+                    Object.entries(shoppingCart).map(([key,product])=>(
+                        <ProductCard 
+                            key={key}
+                            product={product} 
+                            className='bg-dark text-white'
+                            style={{width: '100px'}}
+                            value={product.count}
+                            onChange={onProductCountChange}
+                        >
+                            <ProductImage className='custom-image'/>
+                            <ProductButtons 
+                                className='custom-buttons' 
+                                style={{display:'flex', justifyContent:'center'}}/>
+                        </ProductCard>
+                    ))
+                }
+            </div>
+
+            <div>
+                <code>
+                    {JSON.stringify(shoppingCart, null, 5)}
+                </code>
             </div>
         </div>
     )
