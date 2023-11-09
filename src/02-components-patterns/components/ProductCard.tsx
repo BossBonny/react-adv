6c705c26fd296475678a22610d5ab5e2ddfac07c
John <johnari151298@gmail.com>
2023-11-09 14:42:37 +0100

    Component Extend Styles - Listo

--- src/02-components-patterns/components/ProductCard.tsx
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { useProduct } from '../hooks/useProduct';
-import { createContext } from 'react';
+import { ReactElement, createContext } from 'react';
 
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import { Product, ProductContextProps } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css';
 
@@ -9,8 +9,15 @@ import styles from '../styles/styles.module.css';
 export const ProductContext = createContext({} as ProductContextProps);
 const {Provider} = ProductContext;
 
+export interface Props {
+    product: Product;
+    children?: ReactElement | ReactElement[];
+    className?: string;
+    style?: React.CSSProperties;
+}
 
-export const ProductCard = ({children, product}: ProductCardProps) => {
+
+export const ProductCard = ({children, product, className, style}: Props) => {
 
     const {counter, increaseBy} = useProduct();
 
@@ -22,21 +29,11 @@ export const ProductCard = ({children, product}: ProductCardProps) => {
                 product
             }}
         >
-            <div className={styles.productCard}>
-
+            <div 
+                className={`${styles.productCard} ${className}`}
+                style={style}
+            >
                 {children}
-        {/*
-                <ProductImage
-                    img={product.img}
-                />
-
-                <ProductTitle title={product.title}/>
-
-                <ProductButtons 
-                    increaseBy={increaseBy} 
-                    counter={counter}                
-                />
-        */}
             </div>
         </Provider> 
     )
--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,10 +1,10 @@
 import { ProductCard, ProductImage, ProductTitle ,ProductButtons} from "../components"
+import '../styles/custom-styles.css'
 
 const product = {
     id: '1',
     title: 'Coffe Mug-Card',
     img: './coffee-mug.png',
-
 }
 
 
@@ -22,16 +22,40 @@ export const ShoppingPage = () => {
                 }}
             >
 
-                <ProductCard product={product}>
-                    <ProductCard.Image/>
-                    <ProductCard.Title title='EYO'/>
-                    <ProductCard.Buttons/>
+                <ProductCard product={product} className='bg-dark text-white'>
+                    <ProductCard.Image className='custom-image'/>
+                    <ProductCard.Title title='EYO' className='text-bold'/>
+                    <ProductCard.Buttons className='custom-buttons'/>
                 </ProductCard>
 
-                <ProductCard product={product}>
-                    <ProductImage/>
-                    <ProductTitle/>
-                    <ProductButtons/>
+                <ProductCard product={product} className='bg-dark text-white'>
+                    <ProductImage className='custom-image'/>
+                    <ProductTitle className='text-bold' />
+                    <ProductButtons className='custom-buttons'/>
+                </ProductCard>
+
+                <ProductCard 
+                    product={product}
+                    style={{                        
+                        backgroundColor: '#70D1F8'
+                    }}
+                >
+                    <ProductImage
+                        style={{
+                            boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'
+                        }}
+                    />
+                    <ProductTitle 
+                        style={{
+                            fontWeight: 'bold'
+                        }}
+                    />
+                    <ProductButtons
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'end'
+                        }}
+                    />
                 </ProductCard>
 
             </div>
